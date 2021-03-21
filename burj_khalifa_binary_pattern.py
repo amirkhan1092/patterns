41a5aead0fac28fb9495d224fe02245cce027b59
Amir khan <amirkhan1092@gmail.com>
2021-03-21 22:13:18 +0530

    burj binary solution

--- burj_khalifa_binary_pattern.py
+++ burj_khalifa_binary_pattern.py
@@ -1,9 +1,8 @@
-n=int(input('enter the height of the pattern '))
-l=len(str(bin(n)))-2
+n = int(input('enter the height of the pattern '))
+l = len(bin(n))-2
 
 for k in range(n+1):
-    k=bin(k)
-    k=str(k)[2:]
+    k=bin(k)[2:]
     p=len(k)
     print(' '*(l-p)+k)
 
